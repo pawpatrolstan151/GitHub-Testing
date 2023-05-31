123dc7944c152c2a047d112ca14f8c0b9478f06d
YJ Soon <yjsoon@tinkertanker.com>
2023-05-31 13:24:16 +0800

    Added SG flag

--- GitHub Testing/ContentView.swift
+++ GitHub Testing/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Text("🇸🇬")
             Text("🥔")
             Text("Hello, potato!")
             
