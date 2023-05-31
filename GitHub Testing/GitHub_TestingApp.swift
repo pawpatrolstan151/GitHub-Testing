67739e35ce3c774227dae077f1c1c3b060a8fd47
YJ Soon <yjsoon@tinkertanker.com>
2023-05-31 11:08:23 +0800

    Initial Commit

---
+++ GitHub Testing/GitHub_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestingApp.swift
+//  GitHub Testing
+//
+//  Created by YJ Soon on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
